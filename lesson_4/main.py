24ef587c20f39966c045b3e75ca414f2606c0dc2
midix <bodya.podkopaev@yandex.ru>
2022-10-09 13:12:28 +0700

    09 10 22

---
+++ lesson_4/main.py
@@ -0,0 +1,28 @@
+# x="stroka"
+# x1="stroka"
+# name="anton\nanton\nanton"
+# print(name)
+# st="i am big"
+# print(st.upper())
+# st1="I AM SMALL"
+# print(st1.lower())
+# st2="aaaaaaauhfUHHhUIHuhhuiuihhIUHhiuoOIIJIIO"
+# print(st2.capitalize())
+# familia=input("familia:").capitalize()
+# imya=input("imya:").capitalize()
+# otchestvo=input("otchestvo:").capitalize()
+# print(familia,imya[0]+".",otchestvo[0]+".")
+# print(f"{familia} {imya[0]} {otchestvo[0]}")
+# st=input("slovo:")
+# print(st.count("a"))
+# print(st.lower().count("a"))
+# phrase=input("slova:")
+# lst=phrase.split(",")
+# lst.pop(0)
+# print(lst)
+# phrase=input("stroca:")
+# fint=input("zamena:")
+# replase=input("na chto:")
+# print(phrase.replace(fint,replase))
+# phrase=input("stroca:")
+# print(phrase.replace("e","r"))
