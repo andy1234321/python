2fe51f226ec0ff0ee50f2884dc7454327e1dc46a
midix <bodya.podkopaev@yandex.ru>
2022-10-16 13:14:29 +0700

    16.10.2022

---
+++ lesson5/main.py
@@ -0,0 +1,70 @@
+# x=15
+# if x<=10 or x==15:
+#     print("ne bolshe 10 ili 15")
+# else:
+#     print("bolshe 10")
+# x=10
+# print(x==10)
+# print(x==5)
+# x=int(input("vvedite chislo:"))
+# if x <0:
+#     print("otricatilnoe")
+# elif x >0:
+#     print("polozitelnoe")
+# else:
+#     print("nol")
+# year=int(input("vvedi god:"))
+# if year%4==0 and (year % 400==0 or year %100!=0):
+#     print("visokostninko:)")
+# else:
+#     print("ne visokonu
+# number1=int(input("vvedi 1 chislo:"))
+# operation=input("vvedi operaciy(+,-,*,/)")
+# number2=int(input("vvedi 2 chislo:"))
+# lst_operation=["+","-","/","*"]
+# if operation in lst_operation:
+#     if operation=="+":
+#         print(number1+number2)
+#     elif operation=="/":
+#         print(number1/number2)
+#     else:
+#         print()
+# number1=int(input("vvedi 1 chislo:"))
+# number2=int(input("vvedi 2 chislo:"))
+# number3=int(input("vvedi 3 chislo:"))
+# counterplus=0
+# counterminus=0
+# if number1 >0:
+#     counterplus+=1
+# else:
+#     counterminus+=1
+# if number2 > 0:
+#     counterplus += 1
+# else:
+#     counterminus += 1
+# if number3 > 0:
+#      counterplus += 1
+# else:
+#      counterminus += 1
+# print("polozitelnix:",counterplus)
+# print("otrezatelnix:",counterminus)
+# number1=int(input("vvedi 1 chislo:"))
+# number2=int(input("vvedi 2 chislo:"))
+# number3=int(input("vvedi 3 chislo:"))
+# lst=[number1,number2,number3]
+# mini=min(lst)
+# maxi=max(lst)
+# print("minimalnoe:",mini)
+# print("maximalnoe:",maxi)
+# bilet=input("vvedi nomer bileta:")
+# if len(bilet)==6:
+#     first_half=bilet[:3]
+#     second_half=bilet[-3:]
+#     first_sum=int(first_half[0])+int(first_half[1])+int(first_half[2])
+#     second_sum=int(second_half[0])+int(second_half[1])+int(second_half[2])
+#     if first_sum==second_sum:
+#         print("schastie")
+#     else:
+#         print("neschastie")
+# else:
+#     print("ne to ti vvel")
