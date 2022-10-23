a6fbdcf459dd6ffa2794c87c1e4af2162f004c95
midix <bodya.podkopaev@yandex.ru>
2022-10-23 12:49:17 +0700

    23 10 22

---
+++ lesson 7/main.py
@@ -0,0 +1,17 @@
+# x=15/0(ZeroDivisionError)
+# x=15+"a"(TypeError)
+# x=[10,5,3]
+# print(x[3])(IndexError)
+# x="hello world.(SyntaxError)
+# x=int("a")(ValueError)
+
+# assert
+
+# def summa(number:list[int]):
+#     return  sum(number)
+# assert summa([1,3]) == 5(AssertionError)
+# assert summa([1,3]) == 4(vse horosho)
+# try:
+#     x=5/0
+# except:
+#     print("oshibka")
---
+++ lesson6/main.py
@@ -0,0 +1,65 @@
+# x=["anton","danil","kiril"]
+# x.append("vova")
+# x.append("lena")
+# if len(x) ==3:
+#     print(3)
+# elif len(x)==4:
+#     print(4)
+# else:
+#     print("?")
+# month=int(input("vvedi nomer meseca:"))
+# if month>=1 and month <=12:
+#     if month>= 3 and month<=5:
+#      print("vesna")
+#     elif month>= 6 and month<=8:
+#         print("leto")
+#     elif month>= 9 and month<=11:
+#         print("osen")
+#     else:
+#         print("zima")
+# else:
+#     print("ne to")
+# h = int(input("chasi:"))
+# m = int(input("minuti:"))
+# s = int(input("secundi:"))
+# if 0 <= h <= 23 and 0<= m <=59 and 0<= s <= 59:
+#     print("ok")
+# else:
+#     print("ne ok")
+#     if h < 0 or h > 23:
+#         print("chasi ne corectni")
+#     if m < 0 or m > 59:
+#         print("minuti necorectni")
+#     if s < 0 or s > 59:
+#         print("secundi necorectni")
+# count=0
+# q1=input("planeta zemla kakaya po schetu ot solnca?\n"
+#              "a)2,b)3,v)4,g)5\n"
+#              "-->")
+# if q1 == "b":
+#     count=count+1
+# else:
+#     print("nepravilno")
+#     print("ti nabral 0 balov")
+#     quit()
+#
+# q2=input("scolco pozvonkov u cheloveca?\n"
+#              "a)12,b)8,v)10,g)14\n"
+#              "-->")
+# if q2 == "a":
+#     count=count+1
+# else:
+#     print("nepravilno")
+#     print("ti nabral 1 balov")
+#     quit()
+# q3=input("scolco personagei v dote?\n"
+#              "a)115,b)107,v)95,g)121\n"
+#              "-->")
+# if q3 == "g":
+#     count=count+1
+# else:
+#     print("nepravilno")
+#     print("ti nabral 2 balov")
+#     quit()
+#
+# print("ti vigral tvoi schet:",count)
