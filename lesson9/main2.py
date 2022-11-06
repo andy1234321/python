5ec0826b348e1aca3a9ad20e33a3fe70ffac3700
movavi_school <osetrov.mikhail08@gmail.com>
2022-11-06 13:17:51 +0700

    6 ноября

---
+++ lesson9/main2.py
@@ -0,0 +1,35 @@
+# lst = ["антон", "вова", "гриша", "олег", "ян"]
+# for element in lst:
+#     print(lst)
+# for i in range(0, 10):
+#     print(i)
+# is_game = True
+# while is_game:
+#     i = input("да/нет")
+#     if i == "нет":
+#         is_game = False
+#
+#     print("sue")
+# word = input("введи слово:")
+# while word:
+#     print(word,end=" ")
+#     word = word[:-1]
+# z=int(input("введи число"))
+# while z:
+#     z-=1
+#     if z % 2 ==0:
+#         print(z,end=" ")
+# x = int(input("введите число:"))
+# step=0
+# while x != 1:
+#     step += 1
+#     if x % 2 == 0:
+#         x = x // 2
+#         print(f"{step})", end=" ")
+#         print(x, "/2", end=" ")
+#     else:
+#         print(f"{step})", end=" ")
+#         print(x, "*3+1=", end=" ")
+#         x = 3 * x + 1
+#     print(x)
+# print("шагов:", step)
