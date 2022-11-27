3f5ab5405233230084888d2765acbd356f35286a
movavi_school <andrejcepurnoj3@gmail.com>
2022-11-27 13:14:06 +0700

    27 ноября

--- lesson10/caesar.py
+++ lesson10/caesar.py
@@ -1,5 +1,6 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+symbols = ['(', ')', ',', '.', ' ', '!', '?']
 should_end = False
 while not should_end:
     text = input("введи свое сообщение:").lower()
@@ -9,3 +10,17 @@ while not should_end:
         shift = shift % len(alphabet)
     elif shift < -len(alphabet):
         shift = shift % -len(alphabet)
+    cipher_text = ''
+    for letter in text:
+        if letter in symbols:
+            cipher_text += letter
+        else:
+            position = alphabet.index(letter)
+            if position + shift > len(alphabet):
+                new_position = position + shift - len(alphabet)
+            elif position + shift < 0:
+                new_position = position + shift + len(alphabet)
+            else:
+                new_position = position + shift
+            cipher_text += alphabet[new_position]
+    print(cipher_text)
